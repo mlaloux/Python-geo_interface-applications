fd6093ad6e5d046d06b6d3736b12314516744240
M.Laloux <martin.laloux@gmail.com>
2013-06-30 17:01:42 +0200

    Create ogr_geointerface.py

---
+++ ogr_geointerface.py
@@ -0,0 +1,12 @@
+def records(shapefile):  
+    # generator 
+    reader = ogr.Open(shapefile)
+    layer = ds.GetLayer(0)
+    for i in range(layer.GetFeatureCount()):
+        feature = layer.GetFeature(i)
+        yield json.loads(featurel.ExportToJson())
+
+>>> from osgeo import ogr
+>>> a = records('point.shp')
+>>> a.next()
+{'geometry': {'type': 'Point', 'coordinates': (161821.09375, 79076.0703125)}, 'properties': {'DIP_DIR': 120, 'STRATI_TYP': 1, 'DIP': 30}}
