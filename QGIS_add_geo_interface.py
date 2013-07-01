6bac58197fb858f05a6860347589037f61f8d25d
M.Laloux <martin.laloux@gmail.com>
2013-07-01 09:38:57 +0200

    Create QGIS_add_geo_interface.py

---
+++ QGIS_add_geo_interface.py
@@ -0,0 +1,30 @@
+
+# modified from Nathan Woodrow's [Adding __geo_interface__ to QGIS geometry and feature ](http://nathanw.net/2013/06/25/qgis-geo-interface/)
+# avoid the usage of eval. It is better to use json.loads() to parse JSON Data into dictionary.
+
+
+import json
+
+def mapping_feature(feature):
+    geom = feature.geometry()
+    properties = {}
+    fields = [field.name() for field in feature.fields()]
+    properties = dict(zip(fields, feature.attributes()))
+    return { 'type' : 'Feature',
+             'properties' : properties,
+             'geometry' : geom.__geo_interface__}
+
+def mapping_geometry(geometry):
+    geo = geometry.exportToGeoJSON()
+    # not eval as Nathan Woodrow but json.loads()
+    return json.loads(geo)
+
+QgsFeature.__geo_interface__ = property(lambda self: mapping_feature(self))
+QgsGeometry.__geo_interface__ = property(lambda self: mapping_geometry(self))
+
+layer = iface.activeLayer()
+f = layer.selectedFeatures()[0]
+f
+<qgis.core.QgsFeature object at 0x0D70B8E8>
+f.__geo_interface__
+{'geometry': {u'type': u'Point', u'coordinates': [271066.032148, 154475.631377]}, 'type': 'Feature', 'properties': {u'DIRECTION': 145, u'PENDAGE': 55, u'TYPE': u'incl'}}
