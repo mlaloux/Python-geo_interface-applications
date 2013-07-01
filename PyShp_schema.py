7217ed8ecb6467f84551a0174fdc57048621526b
M.Laloux <martin.laloux@gmail.com>
2013-07-01 13:28:09 +0200

    Update PyShp_schema.py

--- PyShp_schema.py
+++ PyShp_schema.py
@@ -16,3 +16,13 @@ def schema(sf):
 
 print schema(reader)
 {'geometry': 'Point', 'properties': [['PENDAGE', 'N', 2, 0], ['DIRECTION', 'N', 3, 0], ['TYPE', 'C', 10, 0]]}
+
+
+def schema2(layer):
+   tt = dict()
+   tt['geometry'] = reader.shapes()[1].__geo_interface__['type']
+   tt['properties'] =dict((d[0],d[1:]) for d in reader.fields[1:])
+   return tt
+
+schema2(reader)
+{'geometry': 'Point', 'properties': {'DIRECTION': ['N', 3, 0], 'PENDAGE': ['N', 2, 0], 'TYPE': ['C', 10, 0]}}
