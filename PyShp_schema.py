c95f7ccea223937684c741e6738011ec15f2a135
M.Laloux <martin.laloux@gmail.com>
2013-07-01 14:05:41 +0200

    Update PyShp_schema.py

--- PyShp_schema.py
+++ PyShp_schema.py
@@ -8,18 +8,22 @@ c.schema
 
 import shapefile
 reader = shapefile.Reader('strati2.shp')
-def schema(sf):
-   return dict(geometry=sf.shapes()[1].__geo_interface__['type'], properties=sf.fields[1:])
 
-print schema(reader)
-{'geometry': 'Point', 'properties': [['PENDAGE', 'N', 2, 0], ['DIRECTION', 'N', 3, 0], ['TYPE', 'C', 10, 0]]}
+def schema(reader):
+      properties = dict((d[0],d[1:]) for d in reader.fields[1:])
+      return { 'type' : 'Feature',
+               'properties' : properties,
+               'geometry' : reader.shapes()[1].__geo_interface__['type']}
 
+shapefile.Reader.schema = property(lambda self: schema(self))
+reader.schema
+{'geometry': 'Point', 'type': 'Feature', 'properties': {'DIRECTION': ['N', 3, 0], 'PENDAGE': ['N', 2, 0], 'TYPE': ['C
+', 10, 0]}}
 
-def schema2(layer):
-   tt = dict()
-   tt['geometry'] = reader.shapes()[1].__geo_interface__['type']
-   tt['properties'] =dict((d[0],d[1:]) for d in reader.fields[1:])
-   return tt
-
-schema2(reader)
-{'geometry': 'Point', 'properties': {'DIRECTION': ['N', 3, 0], 'PENDAGE': ['N', 2, 0], 'TYPE': ['C', 10, 0]}}
+def schema2(reader):
+      properties = dict((d[0],d[1:]) for d in reader.fields[1:])
+      return {'properties' : properties,
+               'geometry' : reader.shapes()[1].__geo_interface__['type']}
+               
+shapefile.Reader.schema2 = property(lambda self: schema2(self))
+               
