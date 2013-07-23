5a4a9e4e8a6102999165d1e49fcc90d51622bafd
M.Laloux <martin.laloux@gmail.com>
2013-07-23 20:17:16 +0200

    Update pyshp_geojson.py

--- pyshp_geojson.py
+++ pyshp_geojson.py
@@ -1,3 +1,26 @@
+# simple
+
+import shapefile
+
+# read the shapefile
+reader = shapefile.Reader("/Users/Shared/Dropbox/coupe_profile/strati2.shp")>>> fields = reader.fields[1:]
+fields = reader.fields[1:]
+field_names = [field[0] for field in fields]
+buffer = []
+for sr in reader.shapeRecords():
+     atr = dict(zip(field_names, sr.record))
+     geom = sr.shape.__geo_interface__
+     buffer.append(dict(type="Feature", geometry=geom, properties=atr))
+
+# write the GeoJSON file
+from json import dumps
+geojson = open("pyshp-demo.json", "w")
+geojson.write(dumps({"type": "FeatureCollection","features": buffer}, indent=2) + "\n")
+geojson.close()
+
+
+# with coroutines
+
 from json import dumps
     
 def coroutine(func):
